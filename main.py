4939e9bd7d8d3db26b2dd8dd3efc18f23ba774c8
Yabra <bagyar@yandex.ru>
2022-11-17 17:51:59 +0500

    Added random color generation

--- main.py
+++ main.py
@@ -4,37 +4,45 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QWidget
+import UI
 
 
-class Application(QWidget):
+class Application(QWidget, UI.Ui_Form):
     def __init__(self):
         super().__init__()
+        super().setupUi(self)
         self.qp = QPainter()
         self.circles = []
-        uic.loadUi('UI.ui', self)
         self.circlesButton.clicked.connect(self.newCircles)
 
     def paintEvent(self, event):
         self.qp.begin(self)
-        self.qp.setBrush(QColor(255, 255, 0))
 
         for c in self.circles:
-            self.qp.drawEllipse(*c)
+            self.qp.setBrush(QColor(*(c[0])))
+            self.qp.drawEllipse(*(c[1]))
 
         self.qp.end()
 
     def newCircles(self, event):
         for i in range(random.randint(3, 15)):
+            r = random.randint(0, 256)
+            g = random.randint(0, 256)
+            b = random.randint(0, 256)
+
             x = random.randint(0, 500)
             y = random.randint(0, 500)
             size = random.randint(1, 50)
 
             self.circles.append(
                 (
-                    x - size / 2,
-                    y - size / 2,
-                    size,
-                    size
+                    (r, g, b),
+                    (
+                        x - size / 2,
+                        y - size / 2,
+                        size,
+                        size
+                    )
                 )
             )
 
